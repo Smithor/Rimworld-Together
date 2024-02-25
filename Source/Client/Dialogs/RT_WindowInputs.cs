e95d518d9d3506393f29c7837c9e8a4e94926726
AnauNaga <ethanjensen08@gmail.com>
2024-02-24 21:48:33 -0500

    Import Old NagaDev files

---
+++ Source/Client/Dialogs/RT_WindowInputs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+
+namespace GameClient
+{
+    public interface RT_WindowInputs
+    {
+        public abstract void CacheInputs();
+
+        public abstract void SubstituteInputs(List<object> newInputs);
+    }
+}
